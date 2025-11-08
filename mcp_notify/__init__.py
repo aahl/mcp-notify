6838ed3bac5c5718996a97c56aa2c9fa38e660e4
Alone <hi@anlo.ng>
2025-11-08 10:33:42 +0800

    💬 Notify MCP Server

--- mcp_notify/__init__.py
+++ mcp_notify/__init__.py
@@ -15,7 +15,7 @@ from . import (
 _LOGGER = logging.getLogger(__name__)
 
 
-mcp = FastMCP(name="mcp-notify", version="0.1.4")
+mcp = FastMCP(name="mcp-notify", version="0.1.5")
 wework.add_tools(mcp)
 tgbot.add_tools(mcp)
 other.add_tools(mcp)
